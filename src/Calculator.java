54ae0f15e4afbe86461fadd6f3a521fc72d0d440
Sangeetha Justine <sangeetha@techgentsia.com>
2025-08-20 15:55:07 +0530

    Day 2: Simple Calculator

---
+++ src/Calculator.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to the Calculator!");
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        double a = scan.nextDouble();
+
+        System.out.print("Enter second number: ");
+        double b = scan.nextDouble();
+
+        System.out.print("Enter the operator (+ - * /): ");
+        char operator = scan.next().charAt(0);
+
+        double result;
+
+        switch (operator) {
+            case '+':
+                result = a + b;
+                break;
+            case '-':
+                result = a - b;
+                break;
+            case '*':
+                result = a * b;
+                break;
+            case '/':
+                if (b != 0) {
+                    result = a / b;
+                } else {
+                    System.out.println("Error: Division by zero is not allowed.");
+                    return;
+                }
+                break;
+            default:
+                System.out.println("Error: Invalid operator.");
+                return;
+        }
+
+       System.out.println("The result is: " + result);
+    }
+}
