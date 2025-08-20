adcc842530b2d638e777e8f45aba0da59645a810
Sangeetha Justine <sangeetha@techgentsia.com>
2025-08-20 14:56:29 +0530

    Day 1: Hello World

---
+++ src/HelloWorld.java
@@ -0,0 +1,11 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        // Print a welcome message
+        System.out.println("Hello and welcome to the Java world!");
+
+        // Loop through numbers 1 to 5 and print each number
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("i = " + i);
+        }
+    }
+}
